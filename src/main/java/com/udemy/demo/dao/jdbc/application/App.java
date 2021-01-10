6aa38b90ed0adb68511181c9d83b964bfbdf67ae
Igor Pereira <igorpereira120@gmail.com>
2021-01-10 11:53:47 -0300

    Seller entity class

--- src/main/java/com/udemy/demo/dao/jdbc/application/App.java
+++ src/main/java/com/udemy/demo/dao/jdbc/application/App.java
@@ -1,6 +1,9 @@
 package com.udemy.demo.dao.jdbc.application;
 
+import java.util.Date;
+
 import com.udemy.demo.dao.jdbc.model.entities.Department;
+import com.udemy.demo.dao.jdbc.model.entities.Seller;
 
 public class App {
 	
@@ -8,6 +11,9 @@ public class App {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
