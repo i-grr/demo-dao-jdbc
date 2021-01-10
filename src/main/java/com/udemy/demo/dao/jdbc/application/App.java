28ad3d65a68bd4c117376eed117960af2f7e9250
Igor Pereira <igorpereira120@gmail.com>
2021-01-10 13:11:27 -0300

    insert implementation

--- src/main/java/com/udemy/demo/dao/jdbc/application/App.java
+++ src/main/java/com/udemy/demo/dao/jdbc/application/App.java
@@ -1,5 +1,6 @@
 package com.udemy.demo.dao.jdbc.application;
 
+import java.util.Date;
 import java.util.List;
 
 import com.udemy.demo.dao.jdbc.model.dao.DaoFactory;
@@ -29,6 +30,11 @@ public class App {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 04: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 	}
 
--- src/main/java/com/udemy/demo/dao/jdbc/model/dao/impl/SellerDaoJDBC.java
+++ src/main/java/com/udemy/demo/dao/jdbc/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +19,41 @@ import com.udemy.demo.dao.jdbc.model.entities.Seller;
 public class SellerDaoJDBC implements SellerDao {
 
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement preparedStatement = null;
+		
+		try (Connection conn = DB.getConnection()) {
+			
+			String sql = "INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					   + "VALUES (?, ?, ?, ?, ?)";
+			preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			
+			preparedStatement.setString(1, obj.getName());
+			preparedStatement.setString(2, obj.getEmail());
+			preparedStatement.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			preparedStatement.setDouble(4, obj.getBaseSalary());
+			preparedStatement.setInt(5, obj.getDepartment().getId());
+			
+			int rowsAffected = preparedStatement.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet resultSet = preparedStatement.getGeneratedKeys();
+				if (resultSet.next()) {
+					int id = resultSet.getInt(1);
+					obj.setId(id);
+				}
+			}
+				else {
+					throw new DbException("Unexpected error! No rows affected!");
+				}
+			
+		}
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+				finally {
+					DB.closeStatement(preparedStatement);
+				}
 		
 	}
 
